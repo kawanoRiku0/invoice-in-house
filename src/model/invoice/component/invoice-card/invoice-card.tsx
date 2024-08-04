91c9cbee10be6dc2a1a6a9b9b2ddcd965ead402d
kawanoRiku0 <riku.kawano20020518@gmail.com>
2024-08-04 10:11:21 +0900

    feat: 編集導線を作成

--- src/model/invoice/component/invoice-card/invoice-card.tsx
+++ src/model/invoice/component/invoice-card/invoice-card.tsx
@@ -10,87 +10,137 @@ import {
   Table,
   CheckIcon,
   Stack,
+  Group,
+  Box,
+  Button,
+  Menu,
 } from '@mantine/core';
 import { Invoice } from '../../type';
-import { FC, useMemo } from 'react';
+import { FC, useMemo, useState } from 'react';
 import { Icon } from '@iconify/react';
+import { InvoiceCardForm } from './form';
+import { useDisclosure } from '@mantine/hooks';
 
 type Props = {
   invoice: Invoice;
 };
 
-export const InvoiceCard: FC<Props> = ({
-  invoice: { title, items, payment, from, to, createdAt },
-}) => {
+export const InvoiceCard: FC<Props> = ({ invoice }) => {
+  const { title, items, payment, from, to, createdAt } = invoice;
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [isMenuOpen, { open: openMenu, close: closeMenu }] = useDisclosure();
+
   const totalPrice = useMemo(
     () => items.reduce((acc, item) => acc + item.price, 0),
     [items]
   );
   return (
-    <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Flex align="center">
-        {payment ? (
-          <Icon
-            width={24}
-            height={24}
-            icon="mdi:check-circle-outline"
-            color="green"
-          />
-        ) : (
-          <Icon width={24} height={24} icon="mdi:circle-outline" />
-        )}
-        <Text component="p" size="xl" fw="normal">
-          請求書名: <strong>{title}</strong>
-        </Text>
-      </Flex>
-      <Stack>
-        <Text component="p" size="lg" fw={500}>
-          合計 <strong>{totalPrice.toLocaleString()}円</strong>
-        </Text>
-        {payment && (
-          <Text component="h3">
-            支払いURL: <a href={payment.url}>{payment.url}</a>
-          </Text>
-        )}
-        <Accordion chevronPosition="left">
-          <Accordion.Item value="item">
-            <AccordionControl>
-              <Text component="p" size="md">
-                項目一覧
+    <>
+      {isEditing ? (
+        <InvoiceCardForm
+          invoice={invoice}
+          onSubmit={() => setIsEditing(false)}
+        />
+      ) : (
+        <Card shadow="sm" padding="lg" radius="md" withBorder>
+          <Group align="center" justify="space-between">
+            <Group align="center">
+              {payment ? (
+                <Icon
+                  width={24}
+                  height={24}
+                  icon="mdi:check-circle-outline"
+                  color="green"
+                />
+              ) : (
+                <Icon width={24} height={24} icon="mdi:circle-outline" />
+              )}
+              <Text component="p" size="xl" fw="normal">
+                請求書名: <strong>{title}</strong>
               </Text>
-            </AccordionControl>
-            <Accordion.Panel>
-              <Table>
-                <Table.Thead>
-                  <Table.Tr>
-                    <Table.Th>商品名</Table.Th>
-                    <Table.Th>価格</Table.Th>
-                  </Table.Tr>
-                </Table.Thead>
-                <Table.Tbody>
-                  {items.map((item) => (
-                    <Table.Tr key={item.name}>
-                      <Table.Td>{item.name}</Table.Td>
-                      <Table.Td>{item.price}円</Table.Td>
-                    </Table.Tr>
-                  ))}
-                </Table.Tbody>
-              </Table>
-            </Accordion.Panel>
-          </Accordion.Item>
-        </Accordion>
-        <div>
-          <Text component="p" size="sm" c="dimmed">
-            from: {from}
-          </Text>
-          <Text component="p" size="sm" c="dimmed">
-            to: {to}
-          </Text>
-          <Text component="p" size="sm" c="dimmed">
-            createdAt: {createdAt}
-          </Text>
-        </div>
-      </Stack>
-    </Card>
+            </Group>
+            <Menu opened={isMenuOpen} onOpen={openMenu} onClose={closeMenu}>
+              <Menu.Target>
+                <Icon
+                  icon="mdi:dots-vertical"
+                  role="button"
+                  tabIndex={0}
+                  width={18}
+                  height={18}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter' || e.key === ' ') {
+                      e.preventDefault();
+                      openMenu();
+                    }
+                  }}
+                />
+              </Menu.Target>
+              <Menu.Dropdown>
+                <Menu.Item onClick={() => setIsEditing(true)}>
+                  <Button size="xs" variant="transparent">
+                    編集
+                  </Button>
+                </Menu.Item>
+                <Menu.Item onClick={() => setIsEditing(true)}>
+                  <Button size="xs" variant="transparent">
+                    削除
+                  </Button>
+                </Menu.Item>
+              </Menu.Dropdown>
+            </Menu>
+          </Group>
+          <Stack>
+            <Text component="p" size="lg" fw={500}>
+              合計 <strong>{totalPrice.toLocaleString()}円</strong>
+            </Text>
+            {payment && (
+              <Text component="h3">
+                支払いURL: <a href={payment.url}>{payment.url}</a>
+              </Text>
+            )}
+            <Accordion chevronPosition="left">
+              <Accordion.Item value="item">
+                <AccordionControl>
+                  <Text component="p" size="md">
+                    項目一覧
+                  </Text>
+                </AccordionControl>
+                <Accordion.Panel>
+                  <Table>
+                    <Table.Thead>
+                      <Table.Tr>
+                        <Table.Th>商品名</Table.Th>
+                        <Table.Th>価格</Table.Th>
+                      </Table.Tr>
+                    </Table.Thead>
+                    <Table.Tbody>
+                      {items.map((item) => (
+                        <Table.Tr key={item.name}>
+                          <Table.Td>{item.name}</Table.Td>
+                          <Table.Td>{item.price}円</Table.Td>
+                        </Table.Tr>
+                      ))}
+                    </Table.Tbody>
+                  </Table>
+                </Accordion.Panel>
+              </Accordion.Item>
+            </Accordion>
+            <div>
+              <Text component="p" size="sm" c="dimmed">
+                from: {from}
+              </Text>
+              <Text component="p" size="sm" c="dimmed">
+                to: {to}
+              </Text>
+              <Text component="p" size="sm" c="dimmed">
+                createdAt: {createdAt}
+              </Text>
+            </div>
+          </Stack>
+        </Card>
+      )}
+    </>
   );
 };
